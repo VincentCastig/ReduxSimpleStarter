89fedc463ca9195b2df8c518238261d44b35657a
VincentCastig <vincent.castig@gmail.com>
2017-11-06 13:31:30 -0600

    search bar can now search videos

--- src/index.js
+++ src/index.js
@@ -14,11 +14,17 @@ const API_KEY = 'AIzaSyDxl90QCpVnsqlcaZ6ArUA_5OoodZCTILk';
 class App extends Component {
   constructor(props) {
     super(props)
+
     this.state = {
       videos: [],
       selectedVideo: null
-    }
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    };
+
+    this.videoSearch('surfboards'); 
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
@@ -29,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
