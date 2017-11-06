1d445bcb8646e09cf291cd6c6768223cc6047f7e
VincentCastig <vincent.castig@gmail.com>
2017-11-05 22:07:47 -0600

    Console log input value

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
-
+const API_KEY = 'AIzaSyDxl90QCpVnsqlcaZ6ArUA_5OoodZCTILk';
 // import App from './components/app';
 // import reducers from './reducers';
 
-const App = () => <div>Hi!</div>;
+const App = () => {
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
