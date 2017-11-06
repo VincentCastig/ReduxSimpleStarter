0fc19c23d352dcf2f605809daefcb27e7ddc96fb
VincentCastig <vincent.castig@gmail.com>
2017-11-05 22:48:59 -0600

    youtube-api-search included

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 // import { Provider } from 'react-redux';
@@ -8,12 +9,25 @@ const API_KEY = 'AIzaSyDxl90QCpVnsqlcaZ6ArUA_5OoodZCTILk';
 // import App from './components/app';
 // import reducers from './reducers';
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      videos: []
+    }
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
